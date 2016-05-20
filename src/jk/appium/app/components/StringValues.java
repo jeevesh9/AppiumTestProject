135b276049b9e0b4b6595115450a4cf51872c8ee
Jeevesh K <jeeveshwiz@gmail.com>
2016-05-20 13:49:50 +0530

    New version number added

---
+++ src/jk/appium/app/components/StringValues.java
@@ -0,0 +1,9 @@
+package jk.appium.app.components;
+
+public class StringValues {
+	
+	public String adbpath ="C:\\Android\\android-sdk-windows\\platform-tools\\adb.exe"; // Passing the actual path for adb in process builder
+	public String expectedAppName ="Ventura County Star";
+	public String buildNo = "4.3.181.203";
+
+}
